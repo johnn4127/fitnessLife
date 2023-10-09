0343c6537ef4fed9d2743ee6ec489807a75c79df
johnn4127 <johnn4127@gmail.com>
2023-10-09 08:43:37 -0400

    adds equipment

--- src/components/Equipment.jsx
+++ src/components/Equipment.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 const Equipment = () => {
+
+  //pagination, react countdown, equipment, savedlist, child grandchcild manging state of saving your workouts, pass down headers as usecontext in api key
   return (
-    <div>Equipment</div>
+    <div>Equipment
+
+
+
+      
+    </div>
   )
 }
 
