61a93f39336857ab8173be77a78dc2e01b626020
John Nguyen <johnn4127@gmail.com>
2023-10-12 14:52:11 -0400

    adds useContedxt to homepage (#11)

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import Planner from './Planner'
+import '../styles/homepage.css'
+import { createContext } from 'react'
+import { useState } from 'react'
+
+
+export const Cardio = createContext()
+
+
 const Homepage = () => {
+
+const [endOfWorkout, setEndOfWorkout] = useState('End the workout with walking on the treadmill on a incline of 12 and a speed of 3.0.')
+
+
+
   return (
-    <div>
-      <Planner/>
+    <>
+    <div className='mainContainer'>
+      <div className='leftContainer' >leftside</div>
+      <div >
+        <img className='main-con-img' src="https://tropeaka.com/cdn/shop/articles/main_image_d517c79f-4ec7-4946-bb5e-db7e80623e85_1080x.jpg?v=1571697737" alt="" />
+      </div>
+      <div className='rightContainer'>rightside</div>
     </div>
+    <Cardio.Provider value={endOfWorkout}>
+      <Planner/>
+      </Cardio.Provider>
+    </>
   )
 }
 
