14659ddb97b67b2a7383befb96f16462eaa98244
John Nguyen <johnn4127@gmail.com>
2023-10-13 04:37:42 -0400

    adds more css (#16)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,20 +12,15 @@ const Footer = () => {
 							<path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
 						</svg>
 					</div>
-					<span className="self-center text-2xl font-semibold">FitnessLife</span>
+					<span className="self-center text-2xl font-semibold">Brand name</span>
 				</a>
 			</div>
 			
 		</div>
-		<div className="grid justify-center pt-6 lg:justify-between">
+		<div  className="grid justify-center pt-6 lg:justify-between">
 			<div className="flex flex-col self-center text-sm text-center md:block lg:col-start-1 md:space-x-6">
-				<span>©2023 All rights reserved</span>
-				<a rel="noopener noreferrer" href="#">
-					<span>Privacy policy</span>
-				</a>
-				<a rel="noopener noreferrer" href="#">
-					<span>Terms of service</span>
-				</a>
+				<span >©2023 All rights reserved to John Nguyen</span>
+				
 			</div>
 			<div className="flex justify-center pt-4 space-x-4 lg:pt-0 lg:col-end-13">
 				<a rel="noopener noreferrer" href="#" title="Email" className="flex items-center justify-center w-10 h-10 rounded-full dark:bg-violet-400 dark:text-gray-900">
